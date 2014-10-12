6b9033769c968950fb87661c8c19a7274ef04819
mpxx24 <mpxx24@gmail.com>
2014-10-12 22:58:57 +0200

    odtworza video wybrane przez uzytkownika

---
+++ projekt dotNet/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace projekt_dotNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            VideoWindow.Volume = 100;
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            VideoWindow.Source = new Uri(TextBoxFileDirectory.Text);
+            VideoWindow.Play();
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fdialog = new OpenFileDialog();
+            fdialog.InitialDirectory = @"C:\Users\Mariusz\Desktop\dotNet";
+            fdialog.ShowDialog();
+            TextBoxFileDirectory.Text = fdialog.FileName;
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            VideoWindow.Pause();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            VideoWindow.Stop();
+        }
+    }
+}
